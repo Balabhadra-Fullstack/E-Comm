feed01f4393956673f95049db7f8ff740f0a52ef
Balabhadra <mr.bnb000@gmail.com>
2026-05-13 12:22:24 +0530

    Add cartitems relations

--- src/main/java/com/e_commerce/shop_now/Entity/CartItems.java
+++ src/main/java/com/e_commerce/shop_now/Entity/CartItems.java
@@ -2,9 +2,11 @@ package com.e_commerce.shop_now.Entity;
 import java.math.BigDecimal;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import lombok.Data;
 
@@ -14,10 +16,12 @@ public class CartItems {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @ManyToOne
-    private Product product;
-    @ManyToOne
-    private Cart cart;
+  @ManyToOne(fetch = FetchType.LAZY)
+@JoinColumn(name = "product_id")
+private Product product;
+  @ManyToOne(fetch = FetchType.LAZY)
+@JoinColumn(name = "cart_id")
+private Cart cart;
     private int quantity;
     private BigDecimal unitprice;
     private BigDecimal totalunitprice;
